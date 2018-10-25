e5eb90683d022a25caa976528cc9cb186f6f8320
Rafa Fahd <rahman.fad@tiket.com>
2018-10-25 13:56:55 +0700

    Initial Commit

---
+++ Amphibian/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amphibian
+//
+//  Created by Rafa Fahd on 25/10/18.
+//  Copyright © 2018 Team Work. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
